8dc92bedb3879cc9b5f4cf2d2c378a7b49bfb222
ParthaSarathi <ParthaSarathi@DESKTOP-RF1HAOJ>
2019-02-22 05:12:47 +0530

    Initial Commit

---
+++ src/main/java/info/wisdomin/springadmin/SpringadminApplication.java
@@ -0,0 +1,14 @@
+package info.wisdomin.springadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringadminApplication.class, args);
+	}
+
+}
+
